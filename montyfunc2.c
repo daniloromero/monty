fb1ab17e9ae1039d972e32c72cab52c2a1e1b578
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-05-14 23:38:25 +0000

    fix pall

--- montyfunc2.c
+++ montyfunc2.c
@@ -59,6 +59,8 @@ void pall(stack_t **stack, unsigned int line_number)
 	stack_t *copy = *stack;
 
 	(void)line_number;
+	if (!copy)
+		return;
 	for (; copy; copy = copy->next)
 		printf("%d\n", copy->n);
 }
