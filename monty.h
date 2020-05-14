570e1b6365b16b7ec2b862253348864e544f5235
daniloromero <1601@holbertonschool.com>
2020-05-14 18:15:20 +0000

    add header file

---
+++ monty.h
@@ -0,0 +1,67 @@
+#ifndef MONTY_H
+#define MONTY_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+/**
+ * struct auxiliar - struct for input data managment
+ * @monty_file: input from monty file
+ * @head: pointer to head of list
+ * @matrix: array for coman and data
+ * @coman: string for opcode  read from monty file
+ * @counter: file's line cunter
+ *
+ * Description: storage for input and tokenizer
+ * process for usage in every function
+ */
+typedef struct auxiliar
+{
+	FILE *monty_file;
+	stack_t *head;
+	char **matrix;
+	char coman[20];
+	int counter;
+} linehead;
+extern linehead args;
+char **tokenfunc(char *line);
+void matrixfree(void);
+int funchandler(void);
+void push(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+stack_t *addnode(stack_t **stack, unsigned int line_number);
+void free_dlistint(stack_t *head);
+int list_len(stack_t **stack);
+#endif
